7436558d0bfddbf23a8b41955d122ca428ac1aa9
szajkoggg <szajkoggg@gmail.com>
2023-10-14 15:04:46 +0200

    add header for motor speed changes

--- SlideTableMover/MainWindow.xaml.cs
+++ SlideTableMover/MainWindow.xaml.cs
@@ -9,12 +9,29 @@ namespace SlideTableMover
 
     public partial class MainWindow : Window
     {
-  
+        private double xSpeed = 50;
+        private double ySpeed = 100;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+
+        private void ApplySpeed_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(xSpeedTextBox.Text, out double newXSpeed) && double.TryParse(ySpeedTextBox.Text, out double newYSpeed))
+            {
+                MessageBox.Show("Speed updated.");
+                xSpeed = Math.Max(0, newXSpeed);
+                ySpeed = Math.Max(0, newYSpeed);
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid speed values. Please enter valid numbers.");
+            }
+        }
+        private void WorkPane_MouseDown(object sender, MouseButtonEventArgs e)
         {
             double xClicked = e.GetPosition(canvas).X - movingRectangle.ActualWidth / 2;
             double yClicked = e.GetPosition(canvas).Y - movingRectangle.ActualHeight / 2;
@@ -23,19 +40,14 @@ namespace SlideTableMover
             double currentY = Canvas.GetTop(movingRectangle);
 
             double deltaX = xClicked - currentX;
-            double deltaY = yClicked - currentY;
-
-            double xSpeed = 50; // Speed in the X direction (pixels per second)
-            double ySpeed = 100; // Speed in the Y direction (pixels per second)
+            double deltaY = yClicked - currentY; 
 
             double xDuration = Math.Abs(deltaX) / xSpeed;
             double yDuration = Math.Abs(deltaY) / ySpeed;
 
-            // Create and configure DoubleAnimations for X and Y movement
             DoubleAnimation xAnimation = new DoubleAnimation(xClicked, TimeSpan.FromSeconds(xDuration));
             DoubleAnimation yAnimation = new DoubleAnimation(yClicked, TimeSpan.FromSeconds(yDuration));
 
-            // Set the target properties for the animations
             movingRectangle.BeginAnimation(Canvas.LeftProperty, xAnimation);
             movingRectangle.BeginAnimation(Canvas.TopProperty, yAnimation);
         }
